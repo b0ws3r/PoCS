9e90cbdfc048b31e3e73414b9ae70ae92a8ef9f7
b0ws3r <melissa.westland.16@gmail.com>
2021-10-15 01:14:05 -0400

    It does stuff

--- Assn7/Q_4.py
+++ Assn7/Q_4.py
@@ -1,9 +1,12 @@
 import scipy as sp
 from scipy import special
 import numpy as np
+
+# part a
 # For n → ∞,
 # use some computation tool to determine that
 # α ≈ 1.73 for a= 1. (Recall: we expect α<1 for γ >2)
+
 x = np.linspace(1, 2, 100000)
 result = sp.special.zetac(x)
 
@@ -15,7 +18,6 @@ for idx, val in enumerate(x):
     else:
         filter_arr.append(False)
 filtered_results = result[filter_arr]
-
 
 print(len(filtered_results))
 
--- Assn7/SimulatingPowerLawDistribution.py
+++ Assn7/SimulatingPowerLawDistribution.py
@@ -1,28 +1,79 @@
+import matplotlib
+from typing import List
+import pickle
 import numpy
-from scipy.stats import powerlaw
 
-import matplotlib.pyplot as plt
+from Monktools import plottools
 
-
-# Can do displacement after n steps as a power law dist.
-gamma = 5/2
-a = gamma + 1
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import numpy as np
 
 
-def sample_dist(dist):
+def q_1_a():
+    gamma = 5 / 2
+    norm_constant = (gamma - 1) / (1 - 10_000_000 ** -(gamma - 1))
+
+    def P_k(k):
+        return k ** (-gamma)
+
+    p_k_vals = [P_k(k) for k in range(1, 10_000_000 + 1)]
+    bins = [x / norm_constant for x in p_k_vals]
+
+    def filter_portions(boundary: float):
+        for idx, bin in enumerate(bins):
+            if boundary > bin:  # > bin or greater than sum(portions from 1 to bin)
+                return idx + 1
+
+    def get_power_law_k_value() -> int:
+        boundary = np.random.rand()
+        k = filter_portions(boundary)
+        return k
+
+    def get_max_values(samples: int, sample_size: int) -> List[int]:
+        max_vals = list()
+        for _ in range(samples):
+            values = list()
+            for _ in range(sample_size):
+                # calculate number of k steps you need to cross a random boundary
+                # do this {sample_size} times
+                values.append(get_power_law_k_value())
+            # now get the max in this set
+            max_vals.append(max(values))
+        return max_vals
+
+    samplesize_k_max_values_dict = dict()
+    for exp in range(1, 7):
+        samplesize_k_max_values_dict[10 ** exp] = list()
+    for sample_size in samplesize_k_max_values_dict.keys():
+        print(f"sample size: {sample_size}")
+        samplesize_k_max_values_dict[sample_size] = get_max_values(1000, sample_size)
+    for sample_size in samplesize_k_max_values_dict.keys():
+        fig: plt.Figure = plt.figure()
+        ax: plt.Axes = fig.add_subplot()
+        ax.scatter(list(range(1000)), samplesize_k_max_values_dict[sample_size], s=1)
+        ax.set_title(f"Max Values for Sample Sizes of {sample_size:,}")
+        ax.legend()
+        fig.show()
+        plt.savefig(f"Plots/max_values_{sample_size}.jpg")
+
+    pickle.dump(samplesize_k_max_values_dict, open("save.p", "wb"))
 
 
-    pass
+# Part b - plot expected k_max
+def q_1_b():
+    samplesize_k_max_values_dict = pickle.load(open("save.p", "rb"))
+    expected_k_max_values = []
+    for sample_size in samplesize_k_max_values_dict.keys():
+        expected_k_max = numpy.average(samplesize_k_max_values_dict[sample_size])
+        expected_k_max_values.append(expected_k_max)
+
+    fig, ax = plt.subplots()
+    slope, intercept, r, p, stderr = plottools.plot_fit(ax, np.log10(list(samplesize_k_max_values_dict.keys()))
+                                                        , np.log10(expected_k_max_values))
+    plt.legend([f"line with alpha = {slope}", "Log10 of expected k max values for sample sizes N"], shadow=True)
+    plt.savefig(f"Plots/expected_k_max_fit.jpg")
 
 
-for N in [10, 10**2, 10**3, 10**4, 10**5, 10**6]:
-    r = powerlaw.rvs(a, size=1000)
-
-    # create n sets of samples (s_1, s_2, ..., s_n)
-    # samples = []
-    maximums = []  # maximums should be tuples - i, k_max_i, where k_max_i is the maximum of set i (s_i)
-
-    # for each set s in samples, determine and record the maximum value of the set
-    for i in range(N):
-        maximums[i] = numpy.max(r)
-    # plot i, k_max_i
+q_1_a()
+q_1_b()
